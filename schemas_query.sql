40e049f0f741dc37774cdd1babcd496b4fb1defb
unknown <gnzamudio@gmail.com>
2021-10-11 13:55:01 -0700

    schemas query updated

--- schemas_query.sql
+++ schemas_query.sql
@@ -1,40 +1,42 @@
+CREATE TABLE titles(
+	title_id VARCHAR(255) PRIMARY KEY,
+	title varchar(255) NOT NULL
+);
+
+CREATE TABLE departments(
+	dept_no VARCHAR(255) PRIMARY KEY,
+	dept_name VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE Employees(
 	emp_no INT PRIMARY KEY ,
-	empt_title_id VARCHAR(255) NOT NULL UNIQUE,
+	emp_title_id VARCHAR(255) NOT NULL,
 	birth_date DATE,
 	first_name VARCHAR(255) NOT NULL,
 	last_name VARCHAR(255) NOT NULL,
 	sex VARCHAR(255) NOT NULL,
-	hire_date DATE
+	hire_date DATE,
+	FOREIGN KEY(emp_title_id) REFERENCES titles(title_id)	
 );
 
-CREATE TABLE titles(
-	title_id VARCHAR(255) UNIQUE,
-	title varchar(255) NOT NULL,
-	FOREIGN KEY(title_id) REFERENCES Employees(empt_title_id)
-);
 
 CREATE TABLE salaries(
-	emp_no INT PRIMARY KEY,
+	emp_no INT,
 	salary INT,
-	FOREIGN KEY(emp_no) REFERENCES Employees(emp_no)
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
 );
 
-CREATE TABLE dep_emp(
-	emp_no INT PRIMARY KEY,
-	dept_no INT UNIQUE,
-	FOREIGN KEY(emp_no) REFERENCES Employees(emp_no)
+CREATE TABLE dept_emp(
+	emp_no INT ,
+	dept_no VARCHAR(255),
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY(dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager(
-	emp_no INT PRIMARY KEY,
-	dept_no INT UNIQUE,
+	dept_no VARCHAR(255),
+	emp_no INT,
 	FOREIGN KEY(emp_no) REFERENCES Employees(emp_no),
-	FOREIGN KEY(dept_no) REFERENCES dep_emp(dept_no)
+	FOREIGN KEY(dept_no) REFERENCES departments(dept_no)
 );
 
-CREATE TABLE Departmants(
-	dept_no INT UNIQUE,
-	dept_name VARCHAR(255) NOT NULL,
-	FOREIGN KEY(dept_no) REFERENCES dept_manager(dept_no)
-);
